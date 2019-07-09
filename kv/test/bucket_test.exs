78713529b195d6328de8f722a0bde2c9aea4a23a
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2019-07-09 15:28:31 +0200

    Add code from elixir getting started

---
+++ kv/test/bucket_test.exs
@@ -0,0 +1,21 @@
+defmodule KV.BucketTests do
+  use ExUnit.Case, async: true
+  alias KV.Bucket
+
+  setup do
+    {:ok, bucket} = Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores value by key", %{bucket: bucket} do
+    assert Bucket.get(bucket, "milk") == nil
+    Bucket.put(bucket, "milk", 3)
+    assert Bucket.get(bucket, "milk") == 3
+  end
+
+  test "deletes value by key", %{bucket: bucket} do
+    Bucket.put(bucket, "milk", 3)
+    assert Bucket.delete(bucket, "milk") == 3
+    assert Bucket.get(bucket, "milk") == nil
+  end
+end
