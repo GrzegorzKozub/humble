a444b40c6ce9c723afa2eaef4b7ba6079ec6a949
Grzegorz Kozub <grzegorz.kozub@gmail.com>
2019-08-05 08:19:19 +0200

    Implement some Enum functions by hand

--- elxr/lib/lists_and_recursion.ex
+++ elxr/lib/lists_and_recursion.ex
@@ -25,4 +25,42 @@ defmodule ListsAndRecursion do
   def caesar([head | tail], a) do
     [head + a | caesar(tail, a)]
   end
+
+  def all?(list), do: all?(list, fn item -> !!item end)
+
+  def all?([], _func), do: true
+
+  def all?([head | tail], func) do
+    if func.(head) do
+      all?(tail, func)
+    else
+      false
+    end
+  end
+
+  def each([], _func), do: :ok
+
+  def each([head | tail], func) do
+    func.(head)
+    each(tail, func)
+  end
+
+  def each(item, func), do: func.(item)
+
+  def filter([], _func), do: []
+
+  def filter([head | tail], func) do
+    if func.(head) do
+      [head | filter(tail, func)]
+    else
+      filter(tail, func)
+    end
+  end
+
+  def split(list, count), do: _split(list, [], count)
+  defp _split([], front, _), do: [Enum.reverse(front), []]
+  defp _split(tail, front, 0), do: [Enum.reverse(front), tail]
+  defp _split([head | tail], front, count), do: _split(tail, [head | front], count - 1)
+
+  def take(list, count), do: hd(split(list, count))
 end
